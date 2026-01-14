5414451f411da8d0c5784e907736c5a8c5184a5f
Visualcodepo <156519759+Dikrey@users.noreply.github.com>
2026-01-14 19:47:34 +0700

    CodingCamp2026
    Added author information and additional console logs for guidance.

--- src/server.js
+++ src/server.js
@@ -1,3 +1,8 @@
+/**
+ * Project: Bookshelf API Pro
+ * Author: Muhammad Raihan
+ */
+
 const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
 
@@ -15,7 +20,19 @@ const init = async () => {
   server.route(routes);
 
   await server.start();
-  console.log(`Server berjalan pada ${server.info.uri}`);
+
+  console.log(`Server anda berjalan pada ${server.info.uri}`);
+  console.log('-----------------------------------------------------------');
+  console.log('Source code ini dibuat oleh Muhammad Raihan.');
+  console.log('Gunakan hanya untuk pembelajaran, jangan ubah pembuat asli.');
+  console.log('Berkaryalah sesuai ide anda, silakan recode dengan tetap');
+  console.log('mencantumkan nama pembuat asli.');
+  console.log('-----------------------------------------------------------');
 };
+
+process.on('unhandledRejection', (err) => {
+  console.log(err);
+  process.exit(1);
+});
 
 init();
